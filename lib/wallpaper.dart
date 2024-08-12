1ea633b23fa0d73baef1c4a6cb9ab9daad38e9a2
Updesh <theupdesh123@gmail.com>
2024-08-12 23:58:02 +0530

    second commit

--- lib/wallpaper.dart
+++ lib/wallpaper.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:http/http.dart' as http;
 import 'package:wallpaper/fullscreen.dart';
 
@@ -11,16 +12,46 @@ class Wallpaper extends StatefulWidget {
 }
 
 class _WallpaperState extends State<Wallpaper> {
+  void initState() {
+    super.initState();
+    initBannerAd();
+    fetchapi();
+  }
+
+  late BannerAd bannerAd;
+  bool isAdLoaded = false;
+  var adUnit = "ca-app-pub-3940256099942544/9214589741"; // teating id
+
+  initBannerAd() {
+    bannerAd = BannerAd(
+      size: AdSize.banner,
+      adUnitId: adUnit,
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            isAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+          print(error);
+        },
+      ),
+      request: AdRequest(),
+    );
+    bannerAd.load();
+  }
+
   List images = [];
   int page = 1;
   bool isLoading = false;
   bool hasMore = true;
 
-  @override
-  void initState() {
-    super.initState();
-    fetchapi();
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   fetchapi();
+  // }
 
   Future<void> fetchapi() async {
     if (isLoading) return;
@@ -129,6 +160,13 @@ class _WallpaperState extends State<Wallpaper> {
             ),
         ],
       ),
+      bottomNavigationBar: isAdLoaded
+          ? SizedBox(
+              height: bannerAd.size.height.toDouble(),
+              width: bannerAd.size.width.toDouble(),
+              child: AdWidget(ad: bannerAd),
+            )
+          : SizedBox(),
     );
   }
 }
